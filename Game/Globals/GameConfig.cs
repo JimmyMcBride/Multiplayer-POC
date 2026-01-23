8cd7b488201ce0422eb2151ad7cf02f067d16933
Jimmy McBride <mcbride967@gmail.com>
2026-01-22 22:30:41 -0600

    export touch up

--- Game/Globals/GameConfig.cs
+++ Game/Globals/GameConfig.cs
@@ -3,14 +3,18 @@ using MultiplayerPOC.Engine.Core;
 
 namespace MultiplayerPOC.Game.Globals;
 
-public enum GameMode { Client, Server }
+public enum GameMode
+{
+    Client,
+    Server
+}
 
 public partial class GameConfig : Node
 {
     public static GameConfig Instance { get; private set; }
 
     public GameMode Mode { get; private set; } = GameMode.Client;
-    public string ServerAddress { get; private set; } = "127.0.0.1";
+    public string ServerAddress { get; private set; } = "47.160.28.139";
     public int Port { get; private set; } = 7777;
 
     public bool IsServer => Mode == GameMode.Server;
@@ -28,7 +32,6 @@ public partial class GameConfig : Node
         var args = OS.GetCmdlineArgs();
 
         foreach (var arg in args)
-        {
             if (arg == "--server")
             {
                 Mode = GameMode.Server;
@@ -41,26 +44,17 @@ public partial class GameConfig : Node
             {
                 var portStr = arg.Replace("--port=", "");
                 if (int.TryParse(portStr, out var port))
-                {
                     Port = port;
-                }
                 else
-                {
                     Log.Warning($"Invalid port '{portStr}', using default {Port}");
-                }
             }
-        }
     }
 
     private void LogConfiguration()
     {
         if (IsServer)
-        {
             Log.Info($"Starting as DEDICATED SERVER on port {Port}");
-        }
         else
-        {
             Log.Info($"Starting as CLIENT, will connect to {ServerAddress}:{Port}");
-        }
     }
 }
